146f41aa67c024b54ba587786f632a6c2d15f9a6
Atul Jaiswal <atuljaiswal.se@gmail.com>
2026-06-05 10:00:42 +0530

    update next.congfig file

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',       // Tells Next.js to generate static HTML/CSS/JS (into an '/out' folder)
+  images: {
+    unoptimized: true,
+  },
+}
 module.exports = nextConfig
